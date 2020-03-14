61e32da7cbe841bba3d1964a9a4dd02634b7e2c6
Ling Zou <lingzou80@gmail.com>
2020-03-13 22:05:27 -0500

    Add preliminary regression test capability

---
+++ run_test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# run
+./PETScSolver > run_test_output
+
+# do diff for each case
+temp=$(diff output/Solution_step_20.vtk output/expected/Solution_step_20.vtk)
+error=$?
+if [ $error -eq 0 ]
+then
+  printf "................%24b" "\e[0;32m[Step 20; OK]\e[m" "\n"
+else
+  printf "%24b........" "\e[1;31m[Step 20; DIFF ERROR]\e[m" "\n"
+fi
+
+temp=$(diff output/Solution_step_40.vtk output/expected/Solution_step_40.vtk)
+error=$?
+if [ $error -eq 0 ]
+then
+  printf "................%24b" "\e[0;32m[Step 40; OK]\e[m" "\n"
+else
+  printf "%24b........" "\e[1;31m[Step 40; DIFF ERROR]\e[m" "\n"
+fi
